81ef77a1139c499486ff78b4cd0f4d837be5f442
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-11-09 11:39:10 +0100

    initial commit

---
+++ src/main/java/dev/palmes/ilovejava/IlovejavaApplication.java
@@ -0,0 +1,13 @@
+package dev.palmes.ilovejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlovejavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlovejavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/palmes/ilovejava/IlovejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.palmes.ilovejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlovejavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

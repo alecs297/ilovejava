8b050bcc3bc8d130534d2847199c0da2bb066564
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-11-22 23:51:57 +0100

    specified src path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: process.env. NODE_ENV ? 'jit' : undefined,
-  purge: ['./src/main/webapp/**/*.{js,jsp,html}'],
-  content: [],
+  mode: 'jit',
+  content: ['./src/main/webapp/WEB-INF/**/*.{js,jsp,html}'],
   theme: {
     extend: {},
   },

cef8e1a751bca92bd5892dc992dc43fa18131b95
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-11-22 23:02:42 +0100

    tried another way

--- src/main/java/dev/palmes/ilovejava/confs/ApplicationMVCConfig.java
+++ src/main/java/dev/palmes/ilovejava/confs/ApplicationMVCConfig.java
@@ -1,31 +1,24 @@
 package dev.palmes.ilovejava.confs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = { "dev.palmes.ilovejava" })
 public class ApplicationMVCConfig implements WebMvcConfigurer {
-    @Bean
-    public InternalResourceViewResolver resolver() {
-        InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setViewClass(JstlView.class);
-        bean.setPrefix("/WEB-INF/views/");
-        bean.setSuffix(".jsp");
-        return bean;
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views", ".jsp");
     }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/WEB-INF/**").addResourceLocations("classpath://");
-        registry.addResourceHandler("/static/**").addResourceLocations("/WEB-INF/static/");
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
     }
 
 

85adda3ea5552b070dbd5bf8a82f40258ed39c6e
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-11-24 03:17:45 +0100

    added theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   mode: 'jit',
   content: ['./src/main/webapp/WEB-INF/**/*.{js,jsp,html}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "java-pink": "#FC5273",
+        "java-blue": "#2F76B9",
+        "background-default": "#FFFFFF"
+      }
+    },
   },
   plugins: [],
 }

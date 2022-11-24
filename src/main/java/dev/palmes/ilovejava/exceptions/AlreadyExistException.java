577910e3cc080351592708791f253e60a5a909d8
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-11-25 00:00:44 +0100

    exceptions

--- src/main/java/dev/palmes/ilovejava/exceptions/AlreadyExistException.java
+++ src/main/java/dev/palmes/ilovejava/exceptions/AlreadyExistException.java
@@ -1,4 +1,7 @@
 package dev.palmes.ilovejava.exceptions;
 
 public class AlreadyExistException extends Exception {
+    public AlreadyExistException(String message) {
+        super(message);
+    }
 }

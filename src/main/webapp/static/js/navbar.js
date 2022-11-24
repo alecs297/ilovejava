c8104a787f00efb60bcb6f209a0b5c405e23ce53
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-11-24 18:25:56 +0100

    ux improvements (theoretical at least)

--- src/main/webapp/static/js/navbar.js
+++ src/main/webapp/static/js/navbar.js
@@ -3,11 +3,13 @@ const profile_dropdown_content = document.getElementById("profile-dropdown");
 const navbar_toggle = document.getElementById("navbar-toggle");
 const navbar_content = document.getElementById("navbar-content");
 
-profile_dropdown_button.addEventListener("click", function () {
+profile_dropdown_button.addEventListener("mouseup", function (e) {
+    e.preventDefault();
     profile_dropdown_content.classList.toggle("lg:hidden");
 });
 
-navbar_toggle.addEventListener("click", function () {
+navbar_toggle.addEventListener("mouseup", function (e) {
+    e.preventDefault();
     navbar_content.classList.toggle("hidden");
     navbar_toggle.children[0].classList.toggle("invisible");
 });

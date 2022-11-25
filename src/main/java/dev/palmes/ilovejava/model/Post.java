b312cf5a1b7d6607c32d7535865788d402de3f0b
obito1903 <sam200133@gmail.com>
2022-11-25 14:17:45 +0100

    added getContentSummary method

--- src/main/java/dev/palmes/ilovejava/model/Post.java
+++ src/main/java/dev/palmes/ilovejava/model/Post.java
@@ -1,7 +1,9 @@
 package dev.palmes.ilovejava.model;
 
 import javax.persistence.*;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.UUID;
 
 @Entity
@@ -46,9 +48,43 @@ public class Post {
         return content;
     }
 
-    public String getContentSummury() {
-        // TODO
-        return "";
+    /**
+     * Create a summary of the post content.
+     * ie : Shorten the content to a length of maxChar
+     * <ul>
+     *     <li>
+     *         If the content is shorter than maxChar, directly return content.
+     *     </li>
+     *     <li>
+     *         If the content is longer than maxChar, cut at the last word before
+     *         maxChar and append "..." to it.
+     *     </li>
+     * </ul>
+     *
+     * @param maxChar The max number of character wanted
+     */
+    public String getContentSummary(int maxChar) {
+        if (this.getContent().length() < 200) {
+            return this.getContent();
+        }
+
+        String[] words = this.getContent().split(" ");
+        Iterator<String> wordsIt = Arrays.stream(words).iterator();
+        StringBuilder content = new StringBuilder();
+
+        int charCount = 0;
+        while (wordsIt.hasNext() && charCount < maxChar) {
+            String word = wordsIt.next();
+            charCount += word.length();
+            content.append(charCount == 0 ? "" : " ").append(word);
+        }
+
+        if (wordsIt.hasNext()) {
+            content.append("...");
+        }
+
+
+        return content.toString();
     }
 
     public void setContent(String content) {

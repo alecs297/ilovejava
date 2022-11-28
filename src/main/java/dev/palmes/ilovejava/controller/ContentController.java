516edb4eb2b1b158e76ceef526992792266b9a3e
longuetmax <maxime.lngt@gmail.com>
2022-11-28 09:45:05 +0100

    initial post related pages

---
+++ src/main/java/dev/palmes/ilovejava/controller/ContentController.java
@@ -0,0 +1,47 @@
+package dev.palmes.ilovejava.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContentController {
+/*
+    private final ThreadService threadService;
+
+    public ContentController(ThreadService threadService) {
+        this.threadService = threadService;
+    }
+*/
+    /**
+     * GET - Explore page mapping
+     * <p>
+     * Redirect to the "/explore" view
+     * </p>
+     */
+    @GetMapping("/explore")
+    public String explore() {
+        return "content/posts";
+    }
+
+    /**
+     * GET - Explore page mapping
+     * <p>
+     * Redirect to the "/explore" view
+     * </p>
+     */
+    @GetMapping("/trending")
+    public String trending() {
+        return "content/trending";
+    }
+
+    /**
+     * GET - Explore page mapping
+     * <p>
+     * Redirect to the "/explore" view
+     * </p>
+     */
+    @GetMapping("/recent")
+    public String recent() {
+        return "content/recent";
+    }
+}

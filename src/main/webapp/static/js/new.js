f51f7be2cbe00ff4499c0b870c679ead48f4ce5b
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2022-12-05 00:48:07 +0100

    markdown preview

---
+++ src/main/webapp/static/js/new.js
@@ -0,0 +1,15 @@
+const preview_btn = document.getElementById("preview-btn");
+const preview_div = document.getElementById("preview-content");
+const content_div = document.getElementById("content");
+const md = window.markdownit({
+    html: true,
+    linkify: true,
+    typographer: true
+});
+preview_btn.addEventListener("click", () => {
+    preview_div.classList.toggle("hidden");
+    content_div.classList.toggle("hidden");
+    if (preview_btn.checked) {
+        preview_div.innerHTML = md.render(DOMPurify.sanitize(content_div.value));
+    }
+})

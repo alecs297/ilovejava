cf0cc32d75a5e78f0f7249985e80d7c74c616c59
obito1903 <sam200133@gmail.com>
2022-12-20 15:31:04 +0100

    Added Thread DELETE and GET endpoints

--- src/main/java/dev/palmes/ilovejava/controller/ThreadController.java
+++ src/main/java/dev/palmes/ilovejava/controller/ThreadController.java
@@ -1,14 +1,25 @@
 package dev.palmes.ilovejava.controller;
 
+import dev.palmes.ilovejava.exceptions.NotAvailableException;
+import dev.palmes.ilovejava.exceptions.NotFoundException;
+import dev.palmes.ilovejava.exceptions.PermissionLevelException;
 import dev.palmes.ilovejava.model.Post;
 import dev.palmes.ilovejava.model.Thread;
 import dev.palmes.ilovejava.model.User;
 import dev.palmes.ilovejava.service.PostService;
 import dev.palmes.ilovejava.service.ThreadService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.UUID;
 
 @Controller
 public class ThreadController {
@@ -19,6 +30,28 @@ public class ThreadController {
     public ThreadController(ThreadService threadService, PostService postService) {
         this.threadService = threadService;
         this.postService = postService;
+    }
+
+    /**
+     * GET - Thread
+     */
+    @GetMapping("/threads/{id}")
+    public ModelAndView thread(@PathVariable String id, Model model, HttpSession session) {
+        //TODO: Change view path
+        ModelAndView modelAndView = new ModelAndView("thread/thread");
+
+        UUID uuid = UUID.fromString(id);
+
+        try {
+
+            Thread thread = this.threadService.get(uuid);
+            modelAndView.addObject("thread", thread);
+        } catch (NotFoundException | NotAvailableException e) {
+            modelAndView.setViewName("errors/notAvailable");
+            modelAndView.setStatus(HttpStatus.NOT_FOUND);
+            modelAndView.addObject("error", e.getMessage());
+        }
+        return modelAndView;
     }
 
     /**
@@ -80,7 +113,27 @@ public class ThreadController {
     /**
      * DELETE - Delete a Thread
      * <p>
-     *     Mark a thread as deleted
+     * Mark a thread as deleted
      * </p>
      */
+    @DeleteMapping("/threads/{id}")
+    public String deleteThread(Thread thread, Model model, HttpServletResponse response, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/login";
+        }
+        if (thread == null) {
+            return "redirect:/explore";
+        }
+        try {
+            threadService.delete(thread, user);
+        } catch (PermissionLevelException e) {
+            model.addAttribute("error", e.getMessage());
+            response.setStatus(403);
+            return "redirect:/error/notPermitted";
+        }
+
+        return "redirect:/explore";
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/views/errors/notPermitted.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <jsp:include page="/WEB-INF/includes/head.jsp"/>
+    <body>
+        <jsp:include page="/WEB-INF/includes/navbar/navbar.jsp"/>
+        <div class="w-screen text-center my-48 lg:my-28">
+            <img class="mx-auto pointer-events-none w-2/3 lg:w-1/4 aspect-square" src="/static/images/has-bean.png" alt="Content not found error icon"/>
+            <h1 class="my-16 lg:my-8 text-5xl lg:text-xl font-bold">${empty error ? "Sorry, you don't have the permission to do that" : error}</h1>
+            <span class="text-4xl lg:text-base">You can always go <a class="underline" href="/">home</a> or <a class="underline" href="/explore">explore</a> new content</span>
+        </div>
+        <jsp:include page="/WEB-INF/includes/footer.jsp"/>
+    </body>
+</html>
